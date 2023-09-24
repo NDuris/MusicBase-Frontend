890d8246d894c31ea8f47015b762933c54256594
Nduris <nduris@hotmail.com>
2023-09-24 15:50:47 +0200

    .

--- MusicBaseFrontendDB/app.js
+++ MusicBaseFrontendDB/app.js
@@ -1,32 +1,82 @@
+// ============ GLOBAL VARIABELS ============ //
+const endpoint = "http://localhost:3333";
+
 document.addEventListener("DOMContentLoaded", () => {
     const searchInput = document.getElementById("searchInput");
+    const searchType = document.getElementById("searchType");
     const searchButton = document.getElementById("searchButton");
+    const resultsTable = document.getElementById("resultsTable");
     const resultsBody = document.getElementById("resultsBody");
 
+    // Add event listener to the search button
     searchButton.addEventListener("click", () => {
-        const searchTerm = searchInput.value.trim(); // Get the search term
+        const searchTerm = searchInput.value;
+        const selectedSearchType = searchType.value;
 
-        // Perform a search here using AJAX/fetch and populate the table with results
-        // For this example, we'll use dummy data
-        const dummyData = [
-            { name: "Song 1", artist: "Artist 1", album: "Album 1", genre: "Pop" },
-            { name: "Song 2", artist: "Artist 2", album: "Album 2", genre: "Rock" },
-            { name: "Song 3", artist: "Artist 3", album: "Album 3", genre: "Hip-Hop" }
-        ];
+        // Define the endpoint based on the selected search type
+        let endpoint = "";
+        switch (selectedSearchType) {
+            case "artist":
+                endpoint = "/artists";
+                break;
+            case "track":
+                endpoint = "/tracks";
+                break;
+            case "album":
+                endpoint = "/albums";
+                break;
+            default:
+                break;
+        }
 
-        // Clear previous results
-        resultsBody.innerHTML = "";
+        // Fetch data from the selected endpoint
+        fetch(`${endpoint}?search=${searchTerm}`)
+            .then((response) => response.json())
+            .then((data) => {
+                // Clear previous search results
+                resultsBody.innerHTML = "";
 
-        // Loop through the dummy data and create table rows
-        dummyData.forEach((item) => {
-            const row = document.createElement("tr");
-            row.innerHTML = `
-                <td>${item.name}</td>
-                <td>${item.artist}</td>
-                <td>${item.album}</td>
-                <td>${item.genre}</td>
-            `;
-            resultsBody.appendChild(row);
-        });
+                // Update table headers based on search type
+                let tableHeaders = "";
+                switch (selectedSearchType) {
+                    case "artist":
+                        tableHeaders = "<th>Name</th><th>Label</th><th>Genres</th>";
+                        break;
+                    case "track":
+                        tableHeaders = "<th>Track Name</th><th>Artist</th><th>Album(s)</th>";
+                        break;
+                    case "album":
+                        tableHeaders = "<th>Album Name</th><th>Artist(s)</th><th>Tracks</th>";
+                        break;
+                    default:
+                        break;
+                }
+                resultsTable.querySelector("thead").innerHTML = tableHeaders;
+
+                // Populate the table with search results
+                data.forEach((result) => {
+                    let rowHTML = "";
+                    switch (selectedSearchType) {
+                        case "artist":
+                            rowHTML = `<td>${result.name}</td><td>${result.labels}</td><td>${result.genres}</td>`;
+                            break;
+                        case "track":
+                            rowHTML = `<td>${result.track_name}</td><td>${result.artist}</td><td>${result.albums}</td>`;
+                            break;
+                        case "album":
+                            rowHTML = `<td>${result.album_name}</td><td>${result.artists}</td><td>${result.tracks}</td>`;
+                            break;
+                        default:
+                            break;
+                    }
+
+                    const row = document.createElement("tr");
+                    row.innerHTML = rowHTML;
+                    resultsBody.appendChild(row);
+                });
+            })
+            .catch((error) => {
+                console.error("Error fetching data:", error);
+            });
     });
 });
