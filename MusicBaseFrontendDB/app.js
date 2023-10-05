b8a5ba2dd0070394909169b57a6fede108eafa4e
Nduris <nduris@hotmail.com>
2023-10-05 14:55:46 +0200

    Adaption to DB backend

--- MusicBaseFrontendDB/app.js
+++ MusicBaseFrontendDB/app.js
@@ -1,5 +1,5 @@
-// ============ GLOBAL VARIABELS ============ //
-const endpoint = "http://localhost:3333";
+// ============ GLOBAL VARIABLES ============ //
+const baseEndpoint = "http://localhost:3334";
 
 document.addEventListener("DOMContentLoaded", () => {
     const searchInput = document.getElementById("searchInput");
@@ -8,75 +8,70 @@ document.addEventListener("DOMContentLoaded", () => {
     const resultsTable = document.getElementById("resultsTable");
     const resultsBody = document.getElementById("resultsBody");
 
-    // Add event listener to the search button
-    searchButton.addEventListener("click", () => {
+    searchButton.addEventListener("click", async () => {
         const searchTerm = searchInput.value;
         const selectedSearchType = searchType.value;
 
-        // Define the endpoint based on the selected search type
-        let endpoint = "";
+        // Define the endpoint for searching
+        const endpoint = `${baseEndpoint}/search?term=${encodeURIComponent(searchTerm)}&type=${selectedSearchType}`;
+
+        try {
+            // Send a GET request to the server using the constructed endpoint
+            const response = await fetch(endpoint);
+
+            if (response.ok) {
+                const searchResults = await response.json();
+                // Process and display searchResults in your table
+                displaySearchResults(searchResults, selectedSearchType);
+            } else {
+                console.error("Failed to fetch data from the server.");
+            }
+        } catch (error) {
+            console.error("An error occurred:", error);
+        }
+    });
+});
+
+function displaySearchResults(results, selectedSearchType) {
+    const tableHeaders = getTableHeaders(selectedSearchType);
+
+    // Clear previous search results
+    resultsBody.innerHTML = "";
+    resultsTable.querySelector("thead").innerHTML = tableHeaders;
+
+    // Populate the table with search results
+    results.forEach((result) => {
+        let rowHTML = "";
+
         switch (selectedSearchType) {
             case "artist":
-                endpoint = "/artists";
+                rowHTML = `<td>${result.name}</td><td>${result.labels.replace(/[\[\]'"]+/g, '')}</td><td>${result.genres.replace(/[\[\]'"]+/g, '')}</td>`;
                 break;
             case "track":
-                endpoint = "/tracks";
+                rowHTML = `<td>${result.track_name}</td><td>${result.name}</td><td>${result.album_name}</td>`;
                 break;
             case "album":
-                endpoint = "/albums";
+                rowHTML = `<td>${result.album_name}</td><td>${result.name}</td><td>${result.num_songs}</td>`;
                 break;
             default:
                 break;
         }
 
-        // Fetch data from the selected endpoint
-        fetch(`${endpoint}?search=${searchTerm}`)
-            .then((response) => response.json())
-            .then((data) => {
-                // Clear previous search results
-                resultsBody.innerHTML = "";
-
-                // Update table headers based on search type
-                let tableHeaders = "";
-                switch (selectedSearchType) {
-                    case "artist":
-                        tableHeaders = "<th>Name</th><th>Label</th><th>Genres</th>";
-                        break;
-                    case "track":
-                        tableHeaders = "<th>Track Name</th><th>Artist</th><th>Album(s)</th>";
-                        break;
-                    case "album":
-                        tableHeaders = "<th>Album Name</th><th>Artist(s)</th><th>Tracks</th>";
-                        break;
-                    default:
-                        break;
-                }
-                resultsTable.querySelector("thead").innerHTML = tableHeaders;
-
-                // Populate the table with search results
-                data.forEach((result) => {
-                    let rowHTML = "";
-                    switch (selectedSearchType) {
-                        case "artist":
-                            rowHTML = `<td>${result.name}</td><td>${result.labels}</td><td>${result.genres}</td>`;
-                            break;
-                        case "track":
-                            rowHTML = `<td>${result.track_name}</td><td>${result.artist}</td><td>${result.albums}</td>`;
-                            break;
-                        case "album":
-                            rowHTML = `<td>${result.album_name}</td><td>${result.artists}</td><td>${result.tracks}</td>`;
-                            break;
-                        default:
-                            break;
-                    }
-
-                    const row = document.createElement("tr");
-                    row.innerHTML = rowHTML;
-                    resultsBody.appendChild(row);
-                });
-            })
-            .catch((error) => {
-                console.error("Error fetching data:", error);
-            });
+        const row = document.createElement("tr");
+        row.innerHTML = rowHTML;
+        resultsBody.appendChild(row);
     });
-});
+}
+
+function getTableHeaders(selectedSearchType) {
+    switch (selectedSearchType) {
+        case "artist":
+            return "<th>Name</th><th>Label</th><th>Genres</th>";
+        case "track":
+            return "<th>Track Name</th><th>Artist</th><th>Album(s)</th>";
+        case "album":
+            return "<th>Album Name</th><th>Artist(s)</th><th>Tracks</th>";
+        default:
+            return "";
+    }
+}
